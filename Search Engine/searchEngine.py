27bb11c5edf9062e8101625fd925dbc954d5342b
Santiago Paiva <santiago.paiva@mail.mcgill.ca>
2013-03-06 12:44:12 -0500

    Added printAllLinks procedure

--- Search Engine/searchEngine.py
+++ Search Engine/searchEngine.py
@@ -3,11 +3,25 @@
 # Version 1.0
 
 #Extract Link function
+
+def printAllLinks(s):
+        while True: 
+                url, endpos = getNextTarget(s)
+                if url:
+                        print url
+                        s = s[endpos:]
+                else:
+                        break
+                        
+
 def getNextTarget(s):
 	startLink = s.find('<a href=')
-	startQuote = s.find('"', startLink)
-	endQuote = s.find('"', startQuote + 1)
-	url = s[startQuote + 1 : endQuote ]
-	return url, endQuote
-	
-	
+
+        #Tests whether no link was found
+	if start_link == -1:
+                return None, 0 
+        startQuote = s.find('"', startLink)
+        endQuote = s.find('"', startQuote + 1)
+        url = s[startQuote + 1 : endQuote ]
+        return url, endQuote
+
