35e663d4a843b45da78c50320cf45d2900868f63
Santiago Paiva <santiago.paiva@mail.mcgill.ca>
2013-03-25 19:49:13 -0400

    Edited crawler.py and search.py

---
+++ Search Engine/crawler.py
@@ -0,0 +1,167 @@
+#------------------------------------------------------------------------
+# Project: Python Search Engine
+# Author: Santiago Paiva
+# Version 1.0
+# File: crawler.py
+#------------------------------------------------------------------------
+
+def getPage(url):
+    """
+        Inputs a URL and ouputs the content of that URL
+    """
+
+    try:
+        import urllib
+        return urllib.urlopen(url).read()
+    except:
+        return ""
+    
+def getNextTarget(page):
+    """
+        Assummes that s is the seed page.
+        Returns the url and the position of the enquote.
+    """
+        
+    startLink = page.find('<a href=')
+
+    #Tests if no link was found
+    if startLink == -1:
+        return None, 0 
+    startQuote = page.find('"', startLink)
+    endQuote = page.find('"', startQuote + 1)
+    url = page[startQuote + 1 : endQuote ]
+    return url, endQuote
+
+
+def splitString(source,splitlist):
+    """
+        inputs: the string to split and a string containing
+        all of the characters considered separators. The
+        procedure should return a list of strings that break
+        the source string up by the characters in the
+        splitlist.
+    """
+    output = []
+    atsplit = True #At a split point
+
+    for char in source:
+        if char in splitlist:
+            atplist = true
+        else:
+            if atsplit:
+                output.append(char)
+                atsplit = False
+            else:
+                #add character to last word
+                output[-1] = output[-1] + char
+    return output
+                
+
+def getAllLinks(page):
+    """
+        Takes seed page url as an input.
+        Returns a list of all the links found on a page.
+    """
+
+    links = [] 
+
+    while True: 
+        url, endpos = getNextTarget(s)
+
+        if url:
+            #Collect a list of URLs found
+            links.append(url)
+            s = s[endpos:]
+        else:
+            break
+    return links                
+
+def addPagetoIndex(index,url,content):
+    """
+        Inputs:
+        -Index
+        -URL (string)
+        -Content (string)
+
+        Updates the index to include all of the word occurences
+        found in the page content by adding the url to the word's
+        associated url list.
+    """
+
+    words = content.split()
+
+    for word in words:
+        addToIndex(index,word,url)
+        
+
+def addToIndex(index,keyword,url):
+    """
+        Inputs:
+        - an index: [[keyword,[[url,count],[url,count],...]],...]
+        - a keyword: String
+        - a url: String
+    """
+
+    if keyword in index:
+        index[keyword].append(url)
+    else:
+        index[keyword] = [url]
+
+def crawlWeb(seed,maxpages):
+    """
+        Takes seed page url as an input. Maxpages is used
+        to stop crawling after we reach a certain amount of pages
+        since it follows a Depth-first search approach. Outputs
+        a list of all the URLs that can be reached by following
+        links starting from the seed page
+
+        Returns an index and a graph. The graph should be a
+        Dictionary where the entries are url(page):[url,url,...]
+    """
+
+    tocrawl = set([seed])
+    crawled = set() #len(crawled) is length of crawled
+    index = {}
+    graph = {}
+    
+    #While there are more pages to crawl
+    while tocrawl:
+         
+        #Picks last page.
+        page = tocrawl.pop()
+
+        #Tests if page was already crawled
+        if page not in crawled and len(crawled) < maxpages:
+
+            content = getPage(page)
+            #Adds all the link targets on this page to tocrawl
+            addPagetoIndex(index,page,content)
+            outlinks = getAllLinks(content)
+            graph[page] = outlinks
+            tocrawl.update(outlinks)
+            #Adds the page to the list of crawled pages
+            crawled.add(page)
+    return index, graph
+
+def computeRanks(graph):
+    d = 0.8 #Damping Factor
+
+    numloops = 10
+
+    ranks ={}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0/npages
+        
+    for i in range(0,numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1-d)/npages
+
+            #Update by summing in the inlink ranks
+            for node in graph:
+                if page in graph[node]:
+                    newrank = newrank + d*(ranks[node] / len(graph)) 
+        newranks[page] = newrank
+        ranks = newranks
+    return ranks
--- Search Engine/search.py
+++ Search Engine/search.py
@@ -2,138 +2,24 @@
 # Project: Python Search Engine
 # Author: Santiago Paiva
 # Version 1.0
+# File: search.py
 #------------------------------------------------------------------------
 
-def getPage(url):
+def lookup(index,keyword):
     """
-        Inputs a URL and ouputs the content of that URL
-    """
+        Inputs:
+        - an index
+        - keyword
 
-    try:
-        import urllib
-        return urllib.urlopen(url).read()
-    except:
-        return ""
+        Outputs a list of the URLs associated with the keyword.
+        If the keyword is not in the index, the output should
+        an empty list
+    """
     
-def getNextTarget(page):
-    """
-        Assummes that s is the seed page.
-        Returns the url and the position of the enquote.
-    """
-        
-    startLink = page.find('<a href=')
-
-    #Tests if no link was found
-    if startLink == -1:
-        return None, 0 
-    startQuote = page.find('"', startLink)
-    endQuote = page.find('"', startQuote + 1)
-    url = page[startQuote + 1 : endQuote ]
-    return url, endQuote
-
-
-def splitString(source,splitlist):
-    """
-        inputs: the string to split and a string containing
-        all of the characters considered separators. The
-        procedure should return a list of strings that break
-        the source string up by the characters in the
-        splitlist.
-    """
-    output = []
-    atsplit = True #At a split point
-
-    for char in source:
-        if char in splitlist:
-            atplist = true
-        else:
-            if atsplit:
-                output.append(char)
-                atsplit = False
-            else:
-                #add character to last word
-                output[-1] = output[-1] + char
-    return output
-                
-
-def getAllLinks(page):
-    """
-        Takes seed page url as an input.
-        Returns a list of all the links found on a page.
-    """
-
-    links = [] 
-
-    while True: 
-        url, endpos = getNextTarget(s)
-
-        if url:
-            #Collect a list of URLs found
-            links.append(url)
-            s = s[endpos:]
-        else:
-            break
-    return links                
-
-def crawlWeb(seed,maxpages):
-    """
-        Takes seed page url as an input. Maxpages is used
-        to stop crawling after we reach a certain amount of pages
-        since it follows a Depth-first search approach. Outputs
-        a list of all the URLs that can be reached by following
-        links starting from the seed page
-
-        Returns an index and a graph. The graph should be a
-        Dictionary where the entries are url(page):[url,url,...]
-    """
-
-    tocrawl = set([seed])
-    crawled = set() #len(crawled) is length of crawled
-    index = {}
-    graph = {}
-    
-    #While there are more pages to crawl
-    while tocrawl:
-         
-        #Picks last page.
-        page = tocrawl.pop()
-
-        #Tests if page was already crawled
-        if page not in crawled and len(crawled) < maxpages:
-
-            content = getPage(page)
-            #Adds all the link targets on this page to tocrawl
-            addPagetoIndex(index,page,content)
-            outlinks = getAllLinks(content)
-            graph[page] = outlinks
-            tocrawl.update(outlinks)
-            #Adds the page to the list of crawled pages
-            crawled.add(page)
-    return index, graph
-
-def computeRanks(graph):
-    d = 0.8 #Damping Factor
-
-    numloops = 10
-
-    ranks ={}
-    npages = len(graph)
-    for page in graph:
-        ranks[page] = 1.0/npages
-        
-    for i in range(0,numloops):
-        newranks = {}
-        for page in graph:
-            newrank = (1-d)/npages
-
-            #Update by summing in the inlink ranks
-            for node in graph:
-                if page in graph[node]:
-                    newrank = newrank + d*(ranks[node] / len(graph)) 
-        newranks[page] = newrank
-        ranks = newranks
-    return ranks
-
+    if keyword in index:
+        return index[keyword]
+    return None
+   
 def luckySearch(index,ranks,keyword):
     """
         Returns the Highest rating page with keyword
@@ -154,53 +40,20 @@ def recordUserClick(index, keyword, url):
             if entry[0] == url:
                 entry[1] = entry[1]+1
 
-def addToIndex(index,keyword,url):
-    """
-        Inputs:
-        - an index: [[keyword,[[url,count],[url,count],...]],...]
-        - a keyword: String
-        - a url: String
-    """
 
-    if keyword in index:
-        index[keyword].append(url)
-    else:
-        index[keyword] = [url]
 
-        
-def lookup(index,keyword):
+def quicksortPages(pages,ranks):
     """
-        Inputs:
-        - an index
-        - keyword
-
-        Outputs a list of the URLs associated with the keyword.
-        If the keyword is not in the index, the output should
-        an empty list
+        Todo 
     """
-    
-    if keyword in index:
-        return index[keyword]
     return None
 
-   
-def addPagetoIndex(index,url,content):
+def orderedSearch(index,ranks,keyword):
     """
-        Inputs:
-        -Index
-        -URL (string)
-        -Content (string)
-
-        Updates the index to include all of the word occurences
-        found in the page content by adding the url to the word's
-        associated url list.
+        Todo
     """
 
-    words = content.split()
-
-    for word in words:
-        addToIndex(index,word,url)
-        
+    return None
 
 def hashString(keyword,buckets):
     """
